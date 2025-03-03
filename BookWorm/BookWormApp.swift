96f4eb7a65487bd8e947b3cef4d16ea4f587fa7a
DavidFerrs <fernandes.-.david@hotmail.com>
2025-03-03 18:31:05 -0300

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by David Ferreira on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
