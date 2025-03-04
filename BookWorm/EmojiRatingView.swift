a948689a8270cdc4b05c603f13143e70e3a36e32
DavidFerrs <fernandes.-.david@hotmail.com>
2025-03-03 23:55:23 -0300

    Add read, create and delete books from model

---
+++ BookWorm/EmojiRatingView.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiRatingView.swift
+//  BookWorm
+//
+//  Created by David Ferreira on 03/03/25.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("☹️")
+        case 2:
+            Text("🥱")
+        case 3:
+            Text("😐")
+        case 4:
+            Text("😊")
+        default:
+            Text("🤯")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 4)
+}
